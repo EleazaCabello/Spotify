2611b04843b18598aa5368a05d4f86551206331b
EleazarCabello-Sistemas <encabello@syntranet.net>
2024-01-19 14:05:59 -0600

    Trabajo

--- ProyectoSpotify/src/app/shared/shared.module.ts
+++ ProyectoSpotify/src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 import { HeaderUserComponent } from '@shared/components/header-user/header-user.component';
 import { MediaPlayerComponent } from '@shared/components/media-player/media-player.component';
+import { CardPlayerComponent } from '@shared/components/card-player/card-player.component';
+import { SectionGenericComponent } from './components/section-generic/section-generic.component';
 
 
 
@@ -10,7 +12,9 @@ import { MediaPlayerComponent } from '@shared/components/media-player/media-play
   declarations: [
     SideBarComponent,
     HeaderUserComponent,
-    MediaPlayerComponent
+    MediaPlayerComponent,
+    CardPlayerComponent,
+    SectionGenericComponent
   ],
   imports: [
     CommonModule
@@ -18,7 +22,9 @@ import { MediaPlayerComponent } from '@shared/components/media-player/media-play
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    CardPlayerComponent,
+    SectionGenericComponent
   ]
 })
 export class SharedModule { }
